82ac9a3482cc7f6b68e6ede6e0fb0ad63f9b9b80
METAORA\paulo.campez <prcampez@gmail.com>
2019-04-02 14:35:47 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TradutorCavales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPt = new System.Windows.Forms.RichTextBox();
+            this.txtCavalo = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cavalar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPt
+            // 
+            this.txtPt.Location = new System.Drawing.Point(25, 12);
+            this.txtPt.Name = "txtPt";
+            this.txtPt.Size = new System.Drawing.Size(763, 172);
+            this.txtPt.TabIndex = 1;
+            this.txtPt.Text = "";
+            this.txtPt.TextChanged += new System.EventHandler(this.txtPt_TextChanged);
+            // 
+            // txtCavalo
+            // 
+            this.txtCavalo.Location = new System.Drawing.Point(25, 243);
+            this.txtCavalo.Name = "txtCavalo";
+            this.txtCavalo.Size = new System.Drawing.Size(763, 195);
+            this.txtCavalo.TabIndex = 2;
+            this.txtCavalo.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCavalo);
+            this.Controls.Add(this.txtPt);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox txtPt;
+        private System.Windows.Forms.RichTextBox txtCavalo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TradutorCavales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var replace = replacingConsonants(txtPt.Text);
+
+            txtCavalo.Text = replace;
+        }
+
+
+        static bool isVowel(char ch)
+        {
+            if (ch != 'a' && ch != 'e' && ch != 'i'
+                && ch != 'o' && ch != 'u')
+                return false;
+
+            return true;
+        }
+
+
+        static String replacingConsonants(String s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                if (isVowel(s[i]))
+                    continue;
+
+                else
+                {
+                    if (s.Length.Equals(i + 1) || s[i + 1] == ' ')
+                    {
+                        continue;
+                    }
+                    else if (s[i] > 'a' && s[i] < 'e')
+                    {
+
+                        if (Math.Abs(s[i] - 'a') > Math.Abs(s[i] - 'e'))
+                            s = s.Substring(0, i) + 'g' + s.Substring(i + 1);
+                        else
+                            s = s.Substring(0, i) + 'g' + s.Substring(i + 1);
+                    }
+                    else if (s[i] > 'e' && s[i] < 'i')
+                    {
+                        if (Math.Abs(s[i] - 'e') > Math.Abs(s[i] - 'i'))
+                            s = s.Substring(0, i) + "gu" + s.Substring(i + 1);
+                        else
+                            s = s.Substring(0, i) + "gu" + s.Substring(i + 1);
+                    }
+                    else if (s[i] > 'i' && s[i] < 'o')
+                    {
+                        if (Math.Abs(s[i] - 'i') > Math.Abs(s[i] - 'o'))
+                            s = s.Substring(0, i) + "gu" + s.Substring(i + 1);
+                        else
+                            s = s.Substring(0, i) + "gu" + s.Substring(i + 1);
+                    }
+                    else if (s[i] > 'o' && s[i] < 'u')
+                    {
+                        if (Math.Abs(s[i] - 'o') > Math.Abs(s[i] - 'u'))
+                            s = s.Substring(0, i) + 'g' + s.Substring(i + 1);
+                        else
+                            s = s.Substring(0, i) + 'g' + s.Substring(i + 1);
+                    }
+
+                    else if (s[i] > 'u')
+                        s = s.Substring(0, i) + 'g' + s.Substring(i + 1);
+                }
+            }
+
+            return s;
+        }
+
+    }
+}
